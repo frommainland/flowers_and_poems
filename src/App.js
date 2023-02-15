d4dece075f14081bfa9662bb895c872842cccaf0
frommainland <caihehuang@gmail.com>
2023-02-15 18:20:38 +0800

    try flowergrid component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "the-new-css-reset/css/reset.css"
 import './rootCSS.scss'
 import './font.css'
 
+import FlowerGrid from "./homePage/FlowerGrid";
+
 
 import AboutPage from "./aboutPage/AboutPage";
 function App() {
     return (
         <div className="App">
-            <AboutPage />
+            {/* <AboutPage /> */}
+            <FlowerGrid />
+            <div style={{ height: '20vh', width: '100vw' }}></div>
         </div>
     );
 }
---
+++ src/homePage/FlowerGrid.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect } from 'react'
+import './FlowerGrid.scss'
+import flower11 from './flowerTest/11.jpg'
+import flower12 from './flowerTest/12.jpg'
+import flower13 from './flowerTest/13.jpg'
+import flower14 from './flowerTest/14.jpg'
+
+import { useMotionValue } from 'framer-motion'
+
+const FlowerGrid = () => {
+	const mouseX = useMotionValue(null)
+	return (
+		<div
+			className="home flower-grid"
+			onMouseMove={(event) => {
+				mouseX.set(event.nativeEvent.x)
+				console.log(mouseX.current)
+			}}
+			onMouseLeave={() => mouseX.set(null)}
+		>
+			<div className="mask">
+				<div
+					className="scaled"
+					style={{ backgroundImage: `url(${flower11})` }}
+				>
+					{/* <img src={flower11} alt="flower" /> */}
+				</div>
+			</div>
+			<div className="mask">
+				<div className="scaled">
+					{/* <img src={flower12} alt="flower" /> */}
+				</div>
+			</div>
+			<div className="mask">
+				<div className="scaled">
+					{/* <img src={flower13} alt="flower" /> */}
+				</div>
+			</div>
+			<div className="mask">
+				<div className="scaled">
+					{/* <img src={flower14} alt="flower" /> */}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default FlowerGrid
