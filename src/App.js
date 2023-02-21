e886e233d924999636982520a61344af81494a74
frommainland <caihehuang@gmail.com>
2023-02-21 16:47:13 +0800

    add loader component before font face loaded

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import "the-new-css-reset/css/reset.css"
 import './rootCSS.scss'
 import './font.css'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
+import { Loader } from "./components/Loader";
 import { HomePage } from "./homePage/HomePage";
 import AboutPage from "./aboutPage/AboutPage";
-
 
 import { atom } from "jotai";
 export const isHomeFooterInViewAtom = atom(false)
 
-
 function App() {
+    const [isFontLoaded, setIsFontLoaded] = useState(false)
+    useEffect(() => {
+        document.fonts.ready.then(() => {
+            setIsFontLoaded(true)
+        })
+    }, [])
+
     return (
         <div className="App">
-            {/* < HomePage /> */}
-            <AboutPage />
+            {isFontLoaded ?
+                (
+                    <HomePage />
+                ) : (
+                    <Loader />
+                )
+            }
         </div>
     );
 }
